ccdd4d000623aff06334ad9d2f5a9a9cfa821d43
Yazmyn Sims <yzsims@ucsc.edu>
2024-11-06 17:01:50 -0800

    Child Puzzle Complete

--- z3demo.js
+++ z3demo.js
@@ -4,14 +4,37 @@ const { Context } = await init();
 const { Solver, Int, And, Or, Distinct } = new Context("main");
 const solver = new Solver();
 
-const x = Int.const('x');  // x is a Z3 integer
+const Bob = Int.const('Bob');  // x is a Z3 integer
+const Mary = Int.const('Mary');
+const Cathy = Int.const('Cathy');
+const Sue = Int.const('Sue');
 
-solver.add(And(x.le(10), x.ge(9)));  // x <= 10, x >=9
+const Dog = 1;
+const Cat = 2;
+const Bird = 3;
+const Fish = 4;
+
+
+
+solver.add(Distinct(Bob, Mary, Cathy, Sue));
+solver.add(Bob.eq(Dog));
+solver.add(Sue.eq(Bird));
+solver.add(Mary.l(Fish));
+
+
+
 
 // Run Z3 solver, find solution and sat/unsat
 console.log(await solver.check());
 
 // Extract value for x
 const model = solver.model();
-const xVal = model.eval(x);
-console.log(`${xVal}`);
+const bobVal = model.eval(Bob);
+const MaryVal = model.eval(Mary);
+const CathyVal = model.eval(Cathy);
+const SueVal = model.eval(Sue);
+
+console.log(`${bobVal}`);
+console.log(`${MaryVal}`);
+console.log(`${CathyVal}`);
+console.log(`${SueVal}`);
